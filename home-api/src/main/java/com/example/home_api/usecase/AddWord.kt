e2494127db841185b8a58e29abc2c034d0dd0678
irelin <dancerinthedark9@yandex.ru>
2025-04-07 12:25:48 +0400

    old usecases removed

--- home-api/src/main/java/com/example/home_api/usecase/AddWord.kt
+++
@@ -1,6 +0,0 @@
-package com.example.home_api.usecase
-
-interface AddWord {
-
-    suspend operator fun invoke(word: String, translation: String)
-}
--- home-impl/src/main/java/com/example/home/domain/GetLastWordsUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.example.home.domain
-
-import com.example.words_impl.repository.WordsRepository
-import javax.inject.Inject
-
-class GetLastWordsUseCase @Inject constructor(private val wordsRepository: WordsRepository){
-    suspend operator fun invoke(count: Int) = wordsRepository.getLast(count)
-}
