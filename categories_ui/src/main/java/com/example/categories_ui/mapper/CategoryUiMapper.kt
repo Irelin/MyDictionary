d4c7b179d75285da66018bf17824da059afaddb0
irelin <dancerinthedark9@yandex.ru>
2025-06-06 10:48:51 +0400

    ui mappers to ui modules moved

---
+++ categories_ui/src/main/java/com/example/categories_ui/mapper/CategoryUiMapper.kt
@@ -0,0 +1,15 @@
+package com.example.categories_ui.mapper
+
+import com.example.categories_api.domain.models.Category
+import com.example.categories_ui.models.CategoryUI
+import javax.inject.Inject
+
+class CategoryUiMapper @Inject constructor() {
+    fun map(category: Category): CategoryUI = category.let {
+        CategoryUI(
+            it.id,
+            it.name,
+            it.wordsCount
+        )
+    }
+}
